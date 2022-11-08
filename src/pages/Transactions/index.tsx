6c6a04a33b49619f39fc39fac8275e1561ab09ee
joaomorais1f <ifsp.joaov@gmail.com>
2022-11-08 19:25:22 -0300

    adding react-hook-form to all forms

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,30 +1,16 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
+
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
+  const { transactions } = useContext(TransactionsContext)
 
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3000/transactions");
-    const data = await response.json();
-    setTransactions(data);
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
 
   return (
     <div>
@@ -42,11 +28,12 @@ export function Transactions() {
                   <td width="50%"> {transaction.description} </td>
                   <td> 
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type == 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td> {transaction.category} </td>
-                  <td> {transaction.createdAt} </td>
+                  <td> {dateFormatter.format(new Date(transaction.createdAt))} </td>
                 </tr>
                 )
               })}
